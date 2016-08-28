5cc0caa654fe6dfcbbabfdd7b1ad97b5c345077b
Arpan B <arpanbhalla@gmail.com>
2016-08-29 09:27:54 +1000

    Login Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+u1 = User.create :name => "Craig", :email => "craigsy@ga.co", :password => "chicken", :password_confirmation => "chicken",:image =>"http://vignette3.wikia.nocookie.net/rambo/images/e/ea/RamboBoat.jpeg/revision/latest/scale-to-width-down/670?cb=20120815000645"
+u2 = User.create :name => "Jonesy", :email => "jonesy@ga.co", :password => "chicken", :password_confirmation => "chicken",:image =>"https://si.wsj.net/public/resources/images/BN-JB749_ARNOLD_12S_20150624123907.jpg"
+u3 = User.create :name => "Badger", :email => "badger@ga.co", :password => "chicken", :password_confirmation => "chicken",:image => "http://cdn2.bigcommerce.com/server4900/364bb/products/111319/images/78053/286329__96524.1342527721.380.500.jpg?c=2"
+p "User count: #{User.all.count}"
