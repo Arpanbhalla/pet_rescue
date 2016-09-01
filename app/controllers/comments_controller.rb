d8f97b54eb51b89f3b34f2bc24a9863d698e30b0
Arpan B <arpanbhalla@gmail.com>
2016-09-01 17:14:18 +1000

    Rectified Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
     @animal = Animal.find params[:animal_id]
     comment= Comment.create(comment_params)
     @animal.comments << comment
+    @current_user.comments << comment
     redirect_to animal_path(@animal)
   end
 
@@ -13,8 +14,16 @@ class CommentsController < ApplicationController
     @comment = @animal.comments.find params[:id]
   end
 
+  def destroy
+    animal = Animal.find params[:id]
+    animal.destroy
+    redirect_to animals_path
+  end
+
+  end
+
   private
      def comment_params
-       params.require(:comment).permit(:body, :animal_id)
+       params.require(:comment).permit(:body, :animal_id, :user_id)
      end
 end
