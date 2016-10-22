ab54a22dedd603d695ed05d89b46063aeed548ff
Arpan B <arpanbhalla@gmail.com>
2016-10-22 12:30:30 +1100

    22 Oct

--- db/migrate/20160831051727_change_animal_image_to_array.rb
+++
@@ -1,6 +0,0 @@
-class ChangeAnimalImageToArray < ActiveRecord::Migration
-  def change
-    remove_column :animals, :images
-    add_column :animals, :image, :text, :array => true, :default => []
-  end
-end
